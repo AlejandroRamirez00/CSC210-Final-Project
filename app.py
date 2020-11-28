23647fc1eb42943fbfe11968f69758569f8eea91
pseudoCalibrator <leahgoodwin10@gmail.com>
2020-11-28 03:55:42 -0500

    User Auth, minus database stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
+from flask_login import login_required, current_user, LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -12,10 +13,25 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-# ---------- signup goes class goes here ---------
+# ---------- user authencation still needs database implementation---------
+login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
 # ---------- db Model probably goes here ---------
+
+
 
 
 @app.errorhandler(404)
@@ -32,3 +48,7 @@ def index():
 	# ----- more db stuff probably goes here ------
 
 	return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+	return render_template('profile.html', name=current_user.name)
