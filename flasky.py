c04e58608d900fdba58ea6c8314ba5f78c1f7ec6
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-12-09 17:30:23 -0500

    fixed structure for heroku

--- flasky.py
+++ flasky.py
@@ -1,8 +1,8 @@
 import os
 import click
 from flask_migrate import Migrate
-from . import create_app, db
-from .models import User, Achievement
+from app import create_app, db
+from app.models import User, Achievement
 
 #app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 app = create_app()
