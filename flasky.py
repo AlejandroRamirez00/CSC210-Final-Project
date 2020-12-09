07964dfa891429d919c831ee90ee23a509ef7dc5
Alejandro Ramirez <aramir10@u.rochester.edu>
2020-12-09 03:24:21 -0500

    updated requirements.txt

--- flasky.py
+++ flasky.py
@@ -6,6 +6,7 @@ from .models import User, Achievement
 
 #app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 app = create_app()
+application = app
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
